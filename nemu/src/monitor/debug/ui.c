b512702356d8376ee11819ce6d9dae03d77b94cb
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 15:19:24 +0000

    >  compile NEMU 3020208104 root Linux 86b10fbf7886 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:19:24 up  2:53,  0 users,  load average: 0.19, 0.04, 0.01

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -35,7 +35,7 @@ static int cmd_c(char *args) {
 static int cmd_q(char *args) {
 	return -1;
 }
-
+static int cmd_si(char *args);
 static int cmd_help(char *args);
 
 static struct {
@@ -46,6 +46,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
+	{ "si", "Run the cpu_exec function for a specified number of steps", cmd_si},
 
 	/* TODO: Add more commands */
 
@@ -75,6 +76,16 @@ static int cmd_help(char *args) {
 	}
 	return 0;
 }
+
+static int cmd_si(char *args) {
+    int step;
+	char *arg = strtok(NULL, " ");
+	if (arg == NULL) step = 1;
+    else sscanf(arg, "%d", &step);
+	cpu_exec(step);
+	return 0;
+}
+
 
 void ui_mainloop() {
 	while(1) {
