697b94c3a87bddc78cf417186c0c56b71578f4d6
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-04 06:29:58 +0000

    >  compile NEMU 3020208104 root Linux 86b10fbf7886 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:29:58 up 18:04,  0 users,  load average: 0.12, 0.07, 0.02

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -244,7 +244,9 @@ uint32_t expr(char *e, bool *success) {
 	}
 
 	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
-	return 0;
+	*success = true;
+	return eval(0, nr_token - 1);
+	// panic("please implement me");
+	// return 0;
 }
 
