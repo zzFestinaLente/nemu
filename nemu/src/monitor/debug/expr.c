1fb04d88691d04b08c32a4c4cf8c04dffc30eef9
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-04 07:09:45 +0000

    >  compile NEMU 3020208104 root Linux 86b10fbf7886 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:09:45 up 18:44,  0 users,  load average: 0.14, 0.06, 0.01

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -142,138 +142,199 @@ bool check_parentheses(int p, int q) {
 		return false;
 	}
 }
-int prior(int ty) {
-	switch(ty) {
-		case '(': return 1;
-		case ')': return 1;
-		case '!': return 2;
-		case DEREF: return 2;
-		case NEGT: return 2;
-		case '*': return 3;
-		case '/': return 3;
-		case '+': return 4;
-		case '-': return 4;
-		case EQ: return 5;
-		case NEQ: return 5;
-		case AND: return 6;
-		case OR: return 7;
-	}
-	return 0;
-}
 
-int eval (int p, int q) {
-	int result;
-	if (p > q) {
-		assert(0);
-	}
-	else if (p == q) {
-		if (tokens[p].type == REGISTER) {
-			if (strcmp(tokens[p].str+1, "eip") == 0)  return cpu.eip;
-			int k;
-			for (k = 0; k < 8; k++) {
-				if (strcmp(tokens[p].str+1, regsl[k]) == 0)
-					return cpu.gpr[k]._32;
-			}
-		}
-		if (tokens[p].type == HEXNUM) {
-			sscanf(tokens[p].str, "%x", &result);
-			return result;
-		}
-		if (tokens[p].type == DECNUM) {
-			sscanf(tokens[p].str, "%d", &result);
-			return result;
-		}
-		if (tokens[p].type == ADDRESS) {
-			sscanf(tokens[p].str, "%x", &result);
-			return swaddr_read(result, 4);
-		}
-	}
-	else if (check_parentheses(p, q) == true) {
-		return eval(p + 1, q - 1);
-	}
-	else {
-		int op = 0;
-		int j, left = 0, cha0 = 0;
-		for (j = p; j <= q; j++) {
-			if (tokens[j].type == '(') left++;
-			else if (tokens[j].type == ')') left--;
-			else if (left == 0 && tokens[j].type != DECNUM && tokens[j].type != HEXNUM && tokens[j].type != ADDRESS && tokens[j].type != REGISTER) {
-				int current_priority = 0;
-				switch (tokens[j].type) {
-					case '(': case ')': current_priority = 1; break;
-					case '!': case DEREF: case NEGT: current_priority = 2; break;
-					case '*': case '/': current_priority = 3; break;
-					case '+': case '-': current_priority = 4; break;
-					case EQ: case NEQ: current_priority = 5; break;
-					case AND: current_priority = 6; break;
-					case OR: current_priority = 7; break;
-					default: current_priority = 0; break;
-				}
+
+// int eval (int p, int q) {
+// 	int result;
+// 	if (p > q) {
+// 		assert(0);
+// 	}
+// 	else if (p == q) {
+// 		if (tokens[p].type == REGISTER) {
+// 			if (strcmp(tokens[p].str+1, "eip") == 0)  return cpu.eip;
+// 			int k;
+// 			for (k = 0; k < 8; k++) {
+// 				if (strcmp(tokens[p].str+1, regsl[k]) == 0)
+// 					return cpu.gpr[k]._32;
+// 			}
+// 		}
+// 		if (tokens[p].type == HEXNUM) {
+// 			sscanf(tokens[p].str, "%x", &result);
+// 			return result;
+// 		}
+// 		if (tokens[p].type == DECNUM) {
+// 			sscanf(tokens[p].str, "%d", &result);
+// 			return result;
+// 		}
+// 		if (tokens[p].type == ADDRESS) {
+// 			sscanf(tokens[p].str, "%x", &result);
+// 			return swaddr_read(result, 4);
+// 		}
+// 	}
+// 	else if (check_parentheses(p, q) == true) {
+// 		return eval(p + 1, q - 1);
+// 	}
+// 	else {
+// 		int op = 0;
+// 		int j, left = 0, cha0 = 0;
+// 		for (j = p; j <= q; j++) {
+// 			if (tokens[j].type == '(') left++;
+// 			else if (tokens[j].type == ')') left--;
+// 			else if (left == 0 && tokens[j].type != DECNUM && tokens[j].type != HEXNUM && tokens[j].type != ADDRESS && tokens[j].type != REGISTER) {
+// 				int current_priority = 0;
+// 				switch (tokens[j].type) {
+// 					case '(': case ')': current_priority = 1; break;
+// 					case '!': case DEREF: case NEGT: current_priority = 2; break;
+// 					case '*': case '/': current_priority = 3; break;
+// 					case '+': case '-': current_priority = 4; break;
+// 					case EQ: case NEQ: current_priority = 5; break;
+// 					case AND: current_priority = 6; break;
+// 					case OR: current_priority = 7; break;
+// 					default: current_priority = 0; break;
+// 				}
 				
-				if (cha0 == 0) {
-					op = j;
-					cha0 = 1;
-				} else {
-					int op_priority = 0;
-					switch (tokens[op].type) {
-						case '(': case ')': op_priority = 1; break;
-						case '!': case DEREF: case NEGT: op_priority = 2; break;
-						case '*': case '/': op_priority = 3; break;
-						case '+': case '-': op_priority = 4; break;
-						case EQ: case NEQ: op_priority = 5; break;
-						case AND: op_priority = 6; break;
-						case OR: op_priority = 7; break;
-						default: op_priority = 0; break;
-					}
+// 				if (cha0 == 0) {
+// 					op = j;
+// 					cha0 = 1;
+// 				} else {
+// 					int op_priority = 0;
+// 					switch (tokens[op].type) {
+// 						case '(': case ')': op_priority = 1; break;
+// 						case '!': case DEREF: case NEGT: op_priority = 2; break;
+// 						case '*': case '/': op_priority = 3; break;
+// 						case '+': case '-': op_priority = 4; break;
+// 						case EQ: case NEQ: op_priority = 5; break;
+// 						case AND: op_priority = 6; break;
+// 						case OR: op_priority = 7; break;
+// 						default: op_priority = 0; break;
+// 					}
 					
-					if (op_priority <= current_priority) {
-						op = j;
-					}
-				}
-			}
-		}
-	// else {
-	// 	int op = 0;
-	// 	int j, left = 0, cha0 = 0;
-	// 	for (j = p; j <= q; j++) {
-	// 		if (tokens[j].type == '(')  left++;
-	// 		else if (tokens[j].type == ')')  left--;
-	// 		else if (left == 0 && tokens[j].type != DECNUM && tokens[j].type != HEXNUM && tokens[j].type != ADDRESS && tokens[j].type != REGISTER) {
-	// 			if (cha0 == 0) {
-	// 				op = j;
-	// 				cha0 = 1;
-	// 			}
-	// 			else {
-	// 				if (prior(tokens[op].type) <= prior(tokens[j].type)) {
-	// 					op = j;
-	// 				}
-	// 			}
-	// 		}
-	// 	}
+// 					if (op_priority <= current_priority) {
+// 						op = j;
+// 					}
+// 				}
+// 			}
+// 		}
 
-		//printf("&&&&&&&&&&&&&&&&&&&%d\n", op);
-		if (tokens[op].type == '!' || tokens[op].type == DEREF || tokens[op].type == NEGT) {
-			if (tokens[op].type == '!')  return !eval(op + 1, q);
-			if (tokens[op].type == DEREF)  return swaddr_read(eval(op + 1, q), 4);
-			if (tokens[op].type == NEGT)  return -eval(op + 1, q);
-		}
-		int val1 = eval(p, op - 1);
-		int val2 = eval(op + 1, q);
-  		switch (tokens[op].type) {
-			case '+': return val1 + val2;
-			case '-': return val1 - val2;
-			case '*': return val1 * val2;
-			case '/': return val1 / val2;
-			case EQ: return val1 == val2;
-			case AND: return val1 && val2;
-			case OR: return val1 || val2;
-			case NEQ: return val1 != val2;
-			default: assert(0);
-		}
-	}
-	return 0;
+
+// 		//printf("&&&&&&&&&&&&&&&&&&&%d\n", op);
+// 		if (tokens[op].type == '!' || tokens[op].type == DEREF || tokens[op].type == NEGT) {
+// 			if (tokens[op].type == '!')  return !eval(op + 1, q);
+// 			if (tokens[op].type == DEREF)  return swaddr_read(eval(op + 1, q), 4);
+// 			if (tokens[op].type == NEGT)  return -eval(op + 1, q);
+// 		}
+// 		int val1 = eval(p, op - 1);
+// 		int val2 = eval(op + 1, q);
+//   		switch (tokens[op].type) {
+// 			case '+': return val1 + val2;
+// 			case '-': return val1 - val2;
+// 			case '*': return val1 * val2;
+// 			case '/': return val1 / val2;
+// 			case EQ: return val1 == val2;
+// 			case AND: return val1 && val2;
+// 			case OR: return val1 || val2;
+// 			case NEQ: return val1 != val2;
+// 			default: assert(0);
+// 		}
+// 	}
+// 	return 0;
+// }
+int get_priority(int type);
+int handle_unary_op(int type, int start, int end);
+int calculate_result(int type, int val1, int val2);
+
+int eval(int p, int q) {
+    int result;
+    
+    if (p > q) {
+        assert(0);
+    } else if (p == q) {
+        switch (tokens[p].type) {
+            case REGISTER:
+                if (strcmp(tokens[p].str + 1, "eip") == 0) return cpu.eip;
+                int k;
+                for (k = 0; k < 8; k++) {
+                    if (strcmp(tokens[p].str + 1, regsl[k]) == 0)
+                        return cpu.gpr[k]._32;
+                }
+                break;
+            case HEXNUM:
+                sscanf(tokens[p].str, "%x", &result);
+                return result;
+            case DECNUM:
+                sscanf(tokens[p].str, "%d", &result);
+                return result;
+            case ADDRESS:
+                sscanf(tokens[p].str, "%x", &result);
+                return swaddr_read(result, 4);
+        }
+    } else if (check_parentheses(p, q)) {
+        return eval(p + 1, q - 1);
+    } else {
+        int op = 0, left = 0, cha0 = 0, j;
+        for (j = p; j <= q; j++) {
+            if (tokens[j].type == '(') {
+                left++;
+            } else if (tokens[j].type == ')') {
+                left--;
+            } else if (left == 0 && tokens[j].type != DECNUM && tokens[j].type != HEXNUM && tokens[j].type != ADDRESS && tokens[j].type != REGISTER) {
+                int current_priority = get_priority(tokens[j].type);
+                if (cha0 == 0) {
+                    op = j;
+                    cha0 = 1;
+                } else if (get_priority(tokens[op].type) <= current_priority) {
+                    op = j;
+                }
+            }
+        }
+
+        if (tokens[op].type == '!' || tokens[op].type == DEREF || tokens[op].type == NEGT) {
+            return handle_unary_op(tokens[op].type, op + 1, q);
+        }
+
+        int val1 = eval(p, op - 1);
+        int val2 = eval(op + 1, q);
+
+        return calculate_result(tokens[op].type, val1, val2);
+    }
+    return 0;
 }
 
+int get_priority(int type) {
+    switch (type) {
+        case '(': case ')': return 1;
+        case '!': case DEREF: case NEGT: return 2;
+        case '*': case '/': return 3;
+        case '+': case '-': return 4;
+        case EQ: case NEQ: return 5;
+        case AND: return 6;
+        case OR: return 7;
+        default: return 0;
+    }
+}
+
+int handle_unary_op(int type, int start, int end) {
+    switch (type) {
+        case '!': return !eval(start, end);
+        case DEREF: return swaddr_read(eval(start, end), 4);
+        case NEGT: return -eval(start, end);
+        default: assert(0); return 0;
+    }
+}
+
+int calculate_result(int type, int val1, int val2) {
+    switch (type) {
+        case '+': return val1 + val2;
+        case '-': return val1 - val2;
+        case '*': return val1 * val2;
+        case '/': return val1 / val2;
+        case EQ: return val1 == val2;
+        case NEQ: return val1 != val2;
+        case AND: return val1 && val2;
+        case OR: return val1 || val2;
+        default: assert(0); return 0;
+    }
+}
 
 
 uint32_t expr(char *e, bool *success) {
