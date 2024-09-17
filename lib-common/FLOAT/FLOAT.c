7292ffcfc465357237d949fda3497fe5ad5b4dd8
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 07:28:33 +0000

    >  compile NEMU 3020208104 root Linux 86b10fbf7886 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:28:33 up  5:23,  0 users,  load average: 0.22, 0.10, 0.03

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,51 +1,56 @@
 #include "FLOAT.h"
 
+#include <stdint.h>
+ 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	int64_t scale = ((int64_t)a * (int64_t)b) >> 16;
+	return scale;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	/* Dividing two 64-bit integers needs the support of another library
-	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
-	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
-	 * here. A "64/32" division is enough.
-	 *
-	 * To perform a "64/32" division, you can use the x86 instruction
-	 * `div' or `idiv' by inline assembly. We provide a template for you
-	 * to prevent you from uncessary details.
-	 *
-	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
-	 *
-	 * If you want to use the template above, you should fill the "???"
-	 * correctly. For more information, please read the i386 manual for
-	 * division instructions, and search the Internet about "inline assembly".
-	 * It is OK not to use the template above, but you should figure
-	 * out another way to perform the division.
-	 */
+    int sign = 1;
+    FLOAT dividend, divisor, quotient, remainder;
 
-	nemu_assert(0);
-	return 0;
+    if (a < 0) { sign = -sign; dividend = -a; } else { dividend = a; }
+    if (b < 0) { sign = -sign; divisor = -b; } else { divisor = b; }
+
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+	int i;
+    for (i = 0; i < 16; i++) {
+        remainder <<= 1;
+        quotient <<= 1;
+        if (remainder >= divisor) {
+            remainder -= divisor;
+            quotient++;
+        }
+    }
+
+    return sign * quotient;
 }
 
 FLOAT f2F(float a) {
-	/* You should figure out how to convert `a' into FLOAT without
-	 * introducing x87 floating point instructions. Else you can
-	 * not run this code in NEMU before implementing x87 floating
-	 * point instructions, which is contrary to our expectation.
-	 *
-	 * Hint: The bit representation of `a' is already on the
-	 * stack. How do you retrieve it to another variable without
-	 * performing arithmetic operations on it directly?
-	 */
+    union {
+        float f;
+        int i;
+    } converter = {a};
 
-	nemu_assert(0);
-	return 0;
+    int value = converter.i;
+    int sign = value & 0x80000000;
+    int exponent = ((value >> 23) & 0xff) - 150;
+    int fraction = (value & 0x7fffff) | (value ? 0x800000 : 0);
+
+    if (exponent < -16) {
+        fraction >>= -16 - exponent;
+    } else if (exponent > -16) {
+        fraction <<= exponent + 16;
+    }
+
+    return sign ? -fraction : fraction;
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	return a>0?a:-a;
 }
 
 /* Functions below are already implemented */
@@ -61,7 +66,7 @@ FLOAT sqrt(FLOAT x) {
 	return t;
 }
 
-FLOAT pow(FLOAT x, FLOAT y) {
+FLOAT pow(FLOAT x, FLOAT y) {  //PA4中会用到，目前不用
 	/* we only compute x^0.333 */
 	FLOAT t2, dt, t = int2F(2);
 
